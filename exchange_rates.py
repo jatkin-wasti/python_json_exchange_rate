905de0aec84f335af2ca3e76fa4ae43cd2031b40
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-10 14:11:39 +0000

    added comments in the python file

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,19 +1,22 @@
-import json
+import json  # importing json so that we can load the json file into a dictionary
 
 
+# Creating our class
 class ExchangeRates:
 
+    # Creating a function to read the json file and return a dictionary with the same information
     def read_file(self):
-        with open("exchange_rates.json") as jsonfile:
+        with open("exchange_rates.json") as jsonfile:  # Opening the file and aliasing the file name
             rates_dict = json.load(jsonfile)  # load() copies the data and stores into a variable
-        return rates_dict
+        return rates_dict  # Returns the dictionary holding all of the json data
 
-
+    # Creating a function to output the exchange rates by iterating through the dictionary
     def rates_by_country(self):
         rates_dict = self.read_file()
         for key in rates_dict['rates']:
             print(f"€1 is worth {rates_dict['rates'][key]} {key}")
 
 
+# Creating an instance of the class to test if it works
 test = ExchangeRates()
 test.rates_by_country()
